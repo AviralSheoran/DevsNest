cba2dae62142f65d8f5e093278224dfc051ad09a
Aviral Sheoran <aviralsheoran.2002@gmail.com>
2021-07-10 13:03:38 +0530

    update index.js

--- Day6/index.js
+++ Day6/index.js
@@ -4,7 +4,7 @@
 function is_array(input) {
   return Array.isArray(input);
 }
-console.log(is_array("w3resource"));
+console.log(is_array("Hello World!"));
 console.log(is_array([1, 2, 4, 0]));
 
 // !Write a JavaScript function to clone an array
