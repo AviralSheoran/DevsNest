16127b947358e8fe425158fa7abaf29d3652948c
Aviral Sheoran <aviralsheoran.2002@gmail.com>
2021-07-13 19:08:10 +0530

    solve till question no. 4

--- Day7 &8/index.js
+++ Day7 &8/index.js
@@ -17,3 +17,21 @@ delete student.rollno;
 console.log(student);
 
 // Answer -3
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+console.log(Object.keys(student).length);
+
+// Answer -4
+var library = [
+  { author: "Bill Gates", title: "The Road Ahead", readingStatus: true },
+  { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
+];
+
+console.log(Object.values(library));
+
+// Answer -5
