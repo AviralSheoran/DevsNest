4f2e769daa2c7e33026f64a6d47d87cb9f1f057b
Aviral Sheoran <aviralsheoran.2002@gmail.com>
2021-07-13 19:06:51 +0530

    solve 3 questions

--- Day7 &8/index.js
+++ Day7 &8/index.js
@@ -1,1 +1,19 @@
+// Answer -1
+var student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
 
+console.log(Object.keys(student));
+
+// Answer -2
+var student = { name: "David Rayy", sclass: "VI", rollno: 12 };
+
+console.log("before deleting", student);
+
+delete student.rollno;
+
+console.log(student);
+
+// Answer -3
