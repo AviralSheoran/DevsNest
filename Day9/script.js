105ee83087eab7f6e6c8b804b696422d61af1613
Aviral Sheoran <aviralsheoran.2002@gmail.com>
2021-07-15 17:02:31 +0530

    trying the video thing

--- Day9/script.js
+++ Day9/script.js
@@ -1,1 +1,12 @@
+const server = {
+  serverName: "devsnest",
+};
 
+const server2 = {
+  serverName: "The developer community",
+};
+function getInvite(a, b) {
+  console.log(this);
+  console.log(a + b);
+}
+getInvite.call(server2, 2, 4);
